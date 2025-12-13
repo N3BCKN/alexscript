9bc36276dc3b8c3d837b1cd36e16fa815150c1e4
H3BCKN <ka.koszewski@gmail.com>
2025-12-13 18:26:24 +0100

    fix translations

--- lib/alexscript/utils/exceptions_translator.rb
+++ lib/alexscript/utils/exceptions_translator.rb
@@ -2,87 +2,57 @@
 
 # This class in responsible for translating Ruby Exceptions from English to Polish and replace them with AS native ones
 module AlexScript
-    module Utils
-      class ExceptionsTranslator    
-				EXCEPTIONS_MAP = {
-					NoMethodError => 'BladMetody',
-					NameError => 'BladNazwy',
-					TypeError => 'BladTypu',
-					ArgumentError => 'BladArgumentu',
-					ZeroDivisionError => 'BladDzieleniaPrzezZero',
-					SyntaxError => 'BladSkladni',
-					StandardError => 'BladWykonania',
-					Exception => 'WyjatekPodstawowy'
-				}.freeze
-  
-				ERROR_MESSAGES = {
-					'Division by zero' => 'Dzielenie przez zero',
-					'Error executing method' => 'blad podczas wykonywania metody',
-					'undefined method' => 'niezdefiniowana metoda',
-					'undefined local variable or method' => 'niezdefiniowana zmienna lokalna lub metoda',
-					'wrong number of arguments' => 'niewlasciwa liczba argumentow',
-					'no implicit conversion of' => 'brak mozliwosci konwersji',
-					"can't convert" => 'nie mozna przekonwertowac',
-					'unexpected token' => 'nieoczekiwany token',
-					'given' => 'dano',
-					'expected' => 'oczekiwano',
-					'stack level too deep' => 'zbyt glebokie zagniezdzenie stosu',
-					'Index out of bounds' => 'Indeks poza zakresem',
-					'Undeclared identifier' => 'Niezadeklarowany identyfikator',
-					'Variable must be declared' => 'Zmienna musi zostać zadeklarowana',
-					'Uninitialized identifier' => 'Niezainicjalizowany identyfikator',
-					'Condition must be boolean or null' => 'Warunek musi byc typu boolean lub null',
-					'Array index must be an integer' => 'Indeks tablicy musi byc liczbą całkowitą',
-					'Object key must be a string' => 'Klucz obiektu musi byc ciagiem znakow',
-					'Undefined key' => 'Niezdefiniowany klucz',
-					'Cannot call method on undefined object' => 'Nie można wywolac metody na niezdefiniowanym obiekcie',
-					'Maximum recursion depth' => 'Maksymalna głębokosc rekurencji',
-					'stack is too deep' => 'zbyt glebokie zagniezdzenie stosu',
-					'exceeded' => 'przekroczono'
-				}.freeze
-  
-				def self.translate(exception, additional_message = nil)
-					if exception.is_a?(AlexScriptError)
-						# Already an AlexScript error, just pass through
-						return exception
-					end
-					
-					original_message = exception.message
-					translated_message = translate_message(original_message)
-					exception_type = find_exception_type(exception)
-					
-					translated_message += " (#{additional_message})" if additional_message
-					
-					line = Utils::ContextTracker.current_line
-					file = Utils::ContextTracker.current_file
-					
-					# Return AS instead of creating Ruby exception instance
-					AlexScriptError.new(exception_type, translated_message, line)
-				end
-  
-				def self.find_exception_type(excp)
-					case excp
-					when NoMethodError then 'BladMetody'
-					when NameError then 'BladNazwy'
-					when TypeError then 'BladTypu'
-					when ArgumentError then 'BladArgumentu'
-					when ZeroDivisionError then 'BladDzieleniaPrzezZero'
-					when SyntaxError then 'BladSkladni'
-					when StandardError then 'BladWykonania'
-					else 'WyjatekPodstawowy'
-					end
-				end
-  
-				def self.translate_message(message)
-					return nil unless message.is_a?(String)
-					
-					translated = message.dup
-					ERROR_MESSAGES.each do |eng, pol|
-						translated.gsub!(eng, pol) if translated.include?(eng)
-					end
-					
-					translated
-				end
+  module Utils
+    class ExceptionsTranslator    
+      # sirect mapping: Ruby exception class → AS exception name
+      EXCEPTION_MAP = {
+        NoMethodError => 'BladMetody',
+        NameError => 'BladNazwy',
+        TypeError => 'BladTypu',
+        ArgumentError => 'BladArgumentu',
+        ZeroDivisionError => 'BladDzieleniaPrzezZero',
+        SyntaxError => 'BladSkladni',
+        RangeError => 'BladZakresu',
+        StandardError => 'BladWykonania'
+      }.freeze
+
+      # message translations (EN → PL)
+      MESSAGE_TRANSLATIONS = {
+        'division by zero' => 'dzielenie przez zero',
+        'undefined method' => 'niezdefiniowana metoda',
+        'undefined local variable or method' => 'niezdefiniowana zmienna lub metoda',
+        'wrong number of arguments' => 'niewlasciwa liczba argumentow',
+        'no implicit conversion' => 'brak konwersji',
+        "can't convert" => 'nie mozna przekonwertowac',
+        'unexpected token' => 'nieoczekiwany token',
+        'given' => 'dano',
+        'expected' => 'oczekiwano',
+        'stack level too deep' => 'zbyt glebokie zagniezdzenie stosu',
+        'index out of bounds' => 'indeks poza zakresem',
+        'out of bounds' => 'poza zakresem'
+      }.freeze
+
+      def self.translate(exception, additional_message = nil)
+        return exception if exception.is_a?(AlexScriptError)
+        
+        exception_type = EXCEPTION_MAP[exception.class] || 'BladWykonania'
+        
+        # translate message
+        translated_message = translate_message(exception.message)
+        translated_message += " (#{additional_message})" if additional_message
+        
+        AlexScriptError.new(exception_type, translated_message, ContextTracker.current_line)
+      end
+
+      def self.translate_message(message)
+        return message unless message.is_a?(String)
+        
+        result = message.dup
+        MESSAGE_TRANSLATIONS.each do |en, pl|
+          result.gsub!(/#{Regexp.escape(en)}/i, pl)
+        end
+        result
       end
     end
   end
+end
